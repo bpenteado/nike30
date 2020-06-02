65d315efffa43734a9a5a020f4c2526bec5ef016
Bernardo Penteado <bernardopbf3@gmail.com>
2020-06-02 10:31:50 +0100

    fixed RW initial price edge case

--- webscraper.py
+++ webscraper.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-def retrieve_current_deals(websites):
+def retrieve_current_deals(websites, gecko_exe_path, gecko_log_path):
 
 	# scrape websites
 	retrieved_deals = []
-	content = scrape_websites(websites)
+	content = scrape_websites(websites, gecko_exe_path, gecko_log_path)
 
 	# retrieve deals from each website
 	for website, content in content.items():
@@ -16,12 +16,12 @@ def retrieve_current_deals(websites):
 	return retrieved_deals
 
 
-def scrape_websites(websites):
+def scrape_websites(websites, gecko_exe_path, gecko_log_path):
 
 	# initialize webdriver
 	options = webdriver.firefox.options.Options()
 	options.headless = True
-	driver = webdriver.Firefox(executable_path="Users/bernardopenteado/.envs/nike30/geckodriver.exe", options=options, service_log_path="Users/bernardopenteado/.envs/nike30/geckodriver.log")
+	driver = webdriver.Firefox(executable_path=gecko_exe_path, options=options, service_log_path=gecko_log_path)
 
 	content_dict = {}
 	for website, url in websites.items():
@@ -52,14 +52,20 @@ def retrieve_rw(content):
 		sizes_tag = deal.find('span', attrs={"class": "sizes"})
 		sizes = sizes_tag.text if sizes_tag is not None else "All"
 
-		# extract pricing information
+		# extract sale pricing information
 		pricing_tag = deal.find('span', attrs={"class": "pricing"})
 		sale_price = pricing_tag.find('span', attrs={"class": "sale"})  # sale price class seems to change in RW
 		if sale_price:
 			sale_price = float(sale_price.text[1:])
 		else:
 			sale_price = float(pricing_tag.find('span', attrs={"class": "price"}).text[1:])
-		initial_price = float(pricing_tag.find('span', attrs={"class": "list strike"}).text[1:])
+
+		# extract initial pricing information
+		initial_price = pricing_tag.find('span', attrs={"class": "list strike"}) # initial price sometimes not available in RW
+		if initial_price:
+			initial_price = float(pricing_tag.find('span', attrs={"class": "list strike"}).text[1:])
+		else:
+			initial_price = sale_price
 
 		# store shoe information
 		new_deal = {
